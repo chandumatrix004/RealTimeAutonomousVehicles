c84b9c1f3ca3515bbe5ef5a66a26263e6e18def0
Chandra Kumar <chandrakumar004@yahoo.com>
2023-10-02 20:44:11 +0530

    Properly commented the main file

--- main.py
+++ main.py
@@ -46,19 +46,18 @@ while True:
     if not ret:
         print("End of the video file...")
         break
-    ############################################################
-    ### Detect the objects in the frame using the YOLO model ###
-    ############################################################
+
+    # Detecting the objects in the frame using the YOLO model
     results = model(frame, stream=True)
 
-    # loop over the results
+    # Loop over the results
     for result in results:
-        # initialize the list of bounding boxes, confidences, and class IDs
+        # Initializing the list of bounding boxes, confidences, and class IDs
         bboxes = []
         confidences = []
         class_ids = []
 
-        # loop over the detections
+        # Loop over the detections
         for data in result.boxes.data.tolist():
             x1, y1, x2, y2, confidence, class_id = data
             x = int(x1)
@@ -67,77 +66,71 @@ while True:
             h = int(y2) - int(y1)
             class_id = int(class_id)
 
-            # filter out weak predictions by ensuring the confidence is
-            # greater than the minimum confidence
+            # Here we're filtering out weak predictions by ensuring the confidence is greater than minimum confidence
             if confidence > conf_threshold:
                 bboxes.append([x, y, w, h])
                 confidences.append(confidence)
                 class_ids.append(class_id)
                 # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    ############################################################
-    ### Track the objects in the frame using DeepSort        ###
-    ############################################################
+    # Tracking the objects in the frame using DeepSort
 
-    # get the names of the detected objects
+    # Extracting the names of the detected objects from class_names array
     names = [class_names[class_id] for class_id in class_ids]
 
-    # get the features of the detected objects
+    # Extracting the features of the detected objects
     features = encoder(frame, bboxes)
-    # convert the detections to deep sort format
+    # converting the detections to deep sort format
     dets = []
     for bbox, conf, class_name, feature in zip(bboxes, confidences, names, features):
         dets.append(Detection(bbox, conf, class_name, feature))
 
-    # run the tracker on the detections
+    # Here we're the tracker on the detections
     tracker.predict()
     tracker.update(dets)
 
-    # loop over the tracked objects
+    # Looping over the tracked objects
     for track in tracker.tracks:
         if not track.is_confirmed() or track.time_since_update > 1:
             continue
 
-        # get the bounding box of the object, the name of the object, and the track id
+        # Here we're getting the bounding box of the object, the name of the object, and the track id
         bbox = track.to_tlbr()
         track_id = track.track_id
         class_name = track.get_class()
-        # convert the bounding box to integers
+        # Converting the bounding box to integers
         x1, y1, x2, y2 = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
 
-        # get the color associated with the class name
+        # Extracting the color associated with the class name
         class_id = class_names.index(class_name)
         color = colors[class_id]
         B, G, R = int(color[0]), int(color[1]), int(color[2])
 
-        # draw the bounding box of the object, the name
-        # of the predicted object, and the track id
+        # Drawing the bounding box of the object, the name of the predicted object, and the track id
         text = "ID: " + str(track_id) + " - " + class_name
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (B, G, R), 3)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (B, G, R), 2)
         cv2.rectangle(frame, (x1 - 1, y1 - 20),
                       (x1 + len(text) * 12, y1), (B, G, R), -1)
         cv2.putText(frame, text, (x1 + 5, y1 - 8),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-    ############################################################
-    ### Some post-processing to display the results          ###
-    ############################################################
+    # Performing some post-processing to display the results
 
-    # end time to compute the fps
+    # Ending the time to compute the fps
     end = datetime.datetime.now()
-    # calculate the frame per second and draw it on the frame
+    # Calculation of the FPS (Frame Per Second) and draw it on the frame
     fps = f"FPS: {1 / (end - start).total_seconds():.2f}"
     cv2.putText(frame, fps, (50, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
 
-    # show the output frame
+    # Showing each output frame
     cv2.imshow("Output", frame)
-    # write the frame to disk
+    # Writing each frame to the disk
     writer.write(frame)
     if cv2.waitKey(1) == ord("q"):
         break
 
-# release the video capture, video writer, and close all windows
+# Here we're releasing the video capture object, video writer, and closing all windows
 video_cap.release()
 writer.release()
 cv2.destroyAllWindows()
